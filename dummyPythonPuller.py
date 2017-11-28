21b65a121d80ed67c7f69e6faee57bfd505b5881
Omer <Omertorrents@gmail.com>
2017-11-28 21:57:00 +0200

    asdf

--- dummyPythonPuller.py
+++ dummyPythonPuller.py
@@ -2,11 +2,12 @@ from flask import Flask, request, session, url_for, redirect, \
      render_template, abort, g, flash, _app_ctx_stack
 
 app = Flask(__name__)
-
+SECRET = "sha1=86262701ee3d6a58a320ebac31bfc7de0cbdf9a4"
 
 @app.route('/getPull', methods = ['GET', 'POST'])
 def hello_world():
     print(request.data)
+    print(request.headers.get("X-Hub-Signature"))
     return 'Hello World!'
 
 
