7668e8d5d1748f06449dca585667e9d0b2faf03d
Omer <Omertorrents@gmail.com>
2017-11-28 22:22:21 +0200

    asdf

--- myUpdateScript.sh
+++ myUpdateScript.sh
@@ -1,6 +1,6 @@
-cd ../flaskTest
+cd ../flaskTest/webSight
 git pull
-kill $(ps aux | grep port=80 | awk '{print $2}')
+kill $(ps aux | grep port=8080 | awk '{print $2}')
 export FLASK_APP=application.py
-flask run --host=0.0.0.0 --port=80 &
+flask run --host=0.0.0.0 --port=8080 &
 disown
