aa63f82487ba59afad56501e6b1aef0d7a40c3e4
Omer <Omertorrents@gmail.com>
2017-11-28 21:43:45 +0200

    asdf

--- dummyPythonPuller.py
+++ dummyPythonPuller.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request, session, url_for, redirect, \
+     render_template, abort, g, flash, _app_ctx_stack
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/getPull')
 def hello_world():
+    print(request.data)
     return 'Hello World!'
 
 
