23403cfbf33a25273a55a686e8d57f26e7c38d4c
Omer <Omertorrents@gmail.com>
2017-11-28 22:38:19 +0200

    asdf

--- myUpdateScript.sh
+++ myUpdateScript.sh
@@ -1,5 +1,5 @@
 cd ../flaskTest/webSight
 git pull
-kill $(ps aux | grep port='application.py' | awk '{print $2}')
+kill $(ps aux | grep 'application.py' | awk '{print $2}')
 python application.py --host=0.0.0.0 --port=8080 &
 disown
