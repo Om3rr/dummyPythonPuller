89d0b3acb23d393e1946bdc52b843e453c44b40d
Omertorren <Omertorrents@gmail.com>
2017-11-29 13:18:55 +0200

    Update myUpdateScript.sh

--- myUpdateScript.sh
+++ myUpdateScript.sh
@@ -1,5 +1,4 @@
 cd ../flaskTest/webSight
 git pull
 kill $(ps aux | grep 'application.py' | awk '{print $2}')
-python application.py --host=0.0.0.0 --port=8080 &
-disown
+python application.py --host=0.0.0.0 --port=8080
