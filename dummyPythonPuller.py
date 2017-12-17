7a677595152a359690f1f9b812e0cf390e054b7b
Omertorren <Omertorrents@gmail.com>
2017-12-17 09:48:39 +0200

    Update dummyPythonPuller.py

--- dummyPythonPuller.py
+++ dummyPythonPuller.py
@@ -2,7 +2,7 @@ from flask import Flask, request, session, url_for, redirect, \
      render_template, abort, g, flash, _app_ctx_stack
 from subprocess import call
 app = Flask(__name__)
-SECRET = "sha1=86262701ee3d6a58a320ebac31bfc7de0cbdf9a4"
+SECRET = "YourSecret."
 
 @app.route('/getPull', methods = ['GET', 'POST'])
 def hello_world():
